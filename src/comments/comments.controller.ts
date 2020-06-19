63c6cc35c7834c93a797b085855c2d1c9ae908b2
jcneg <juan.negrin@electronicid.eu>
2020-06-20 00:15:20 +0200

    Added functionality

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Headers, BadRequestException } from '@nestjs/common';
+import { UserData } from './business/UserData.decorator';
+import { CreateCommentDto } from './dto/CreateCommentDto';
+import { CreateComment } from './business/CreateComment.decorator';
+import { Comments } from './entities/comments.entity';
+import { UserDto } from './dto/UserDto';
+import { CommentsService } from './comments.service';
 
 @Controller('comments')
-export class CommentsController {}
+export class CommentsController {
+
+    constructor(private commentsService: CommentsService) {}
+
+    @Get()
+    async getPostComments(@Headers('authorization') bearer_token: string, @CreateComment() comment: CreateCommentDto): Promise<Comments[]> {
+        if (!comment.userId || !comment.postId || !comment.content) {
+            throw new BadRequestException('Invalid comment data received');
+        }
+
+        const user: UserDto = await this.commentsService.getUserData(bearer_token);
+        return ;
+    }
+}
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Comments } from './entities/comments.entity';
+import { UserDto } from './dto/UserDto';
+import * as jwt from 'jsonwebtoken';
+import { CreateCommentDto } from './dto/CreateCommentDto';
 
 @Injectable()
 export class CommentsService {
@@ -21,4 +24,15 @@ export class CommentsService {
       async remove(id: string): Promise<void> {
         await this.commentsRepository.delete(id);
       }
+
+      async getUserData(authorizationToken: string): Promise<UserDto> {
+        const bearer_token = authorizationToken.split(' ');
+        const token = bearer_token[1];
+        const userInfo: UserDto = await jwt.verify(token, process.env.JWT_SECRET) as UserDto;
+        return userInfo;
+    }
+
+    createComment(user: UserDto, postId: string, comment: CreateCommentDto): Promise<void> {
+        return ;
+    }
 }
